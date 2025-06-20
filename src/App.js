d5b599846441ff43f4133865c31f90cc8662b424
nativosistemas <nativosistemas@outlook.com.ar>
2025-06-20 02:59:42 -0300

    branch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+/*import { AuthProvider } from './context/AuthContext';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App; // Esta línea es crucial
+*/
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,18 +19,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Hola Mundo.
+          Hola Mundo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
