c36afadc9af489a673e4a0f36b7327a15e070c1d
nativosistemas <nativosistemas@outlook.com.ar>
2025-12-27 18:33:46 -0300

    laser03

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/seweb">
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout />}>
